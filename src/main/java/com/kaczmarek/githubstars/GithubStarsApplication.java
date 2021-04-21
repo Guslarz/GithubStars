9e67755c30fafe97ebdcdd704a08d5b943114e44
Guslarz <mrkaczmareks@gmail.com>
2021-04-21 17:36:51 +0200

    Initial

---
+++ src/main/java/com/kaczmarek/githubstars/GithubStarsApplication.java
@@ -0,0 +1,12 @@
+package com.kaczmarek.githubstars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStarsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GithubStarsApplication.class, args);
+  }
+}
---
+++ src/test/java/com/kaczmarek/githubstars/GithubStarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaczmarek.githubstars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubStarsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
